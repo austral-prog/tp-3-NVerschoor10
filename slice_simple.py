db931d0dc96a95a8be0226061e1d520417bbd651
NVerschoor10 <nverschoor@mail.austral.edu.ar>
2025-04-11 12:26:55 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,5 +1,13 @@
 def slice_simple():
     texto = "Awesome"
+    texto = texto.lower()
+    print(texto[0:3])
+    a = int((len(texto)/2)-1)
+    b = int((len(texto)/2)+2)
+    print(texto[a:b])
+    print(texto[0:7])
+   
+
     # Código a implementar, se debe utilizar la variable 'texto' para resolver el ejercicio.
     # No se debe modificar la definición de la función, ni ingresar otro valor mediante input.
 
