50bf967643d4b39b4c5ad2d24ca3f9e1e062f76a
NVerschoor10 <nverschoor@mail.austral.edu.ar>
2025-04-14 18:27:30 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,16 +1,13 @@
 def check_vowels():
-nombre = str(input("Matias")).lower()
-
-
-contiene_a = "a" in nombre
-contiene_e = "e" in nombre
-contiene_i = "i" in nombre
-contiene_o = "o" in nombre
-contiene_u = "u" in nombre
-
-print(f"contiene a: {contiene_a}")
-print(f"contiene e: {contiene_e}")
-print(f"contiene i: {contiene_i}")
-print(f"contiene o: {contiene_o}")
-print(f"contiene u: {contiene_u}")
+  nombre = str(input("Matias")).lower()
+  contiene_a = "a" in nombre
+  contiene_e = "e" in nombre
+  contiene_i = "i" in nombre
+  contiene_o = "o" in nombre
+  contiene_u = "u" in nombre
+  print(f"contiene a: {contiene_a}")
+  print(f"contiene e: {contiene_e}")
+  print(f"contiene i: {contiene_i}")
+  print(f"contiene o: {contiene_o}")
+  print(f"contiene u: {contiene_u}")
 
